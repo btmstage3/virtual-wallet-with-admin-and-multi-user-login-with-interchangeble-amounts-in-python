91315a3c93a3ce9dc06431233ab962c4b9e2f66c
btmstage3@gmail.com <btmstage3@gmail.com>
2023-04-07 15:54:39 +0530

    login_view_changes

--- users/views.py
+++ users/views.py
@@ -1,26 +1,41 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 
-@login_required(login_url='/login/')
+
 def home(request):
     print('Home view accessed.')
     return render(request, 'home.html')
 
+
 def login_view(request):
     print('Login view accessed.')
-    form = AuthenticationForm()
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = AuthenticationForm(request=request, data=request.POST)
         if form.is_valid():
-            login(request, form.get_user(), form.get_password())
-            print('User authenticated.')
-            return redirect('home')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Welcome {username}")
+                return redirect('home')
+            else:
+                messages.error(request, 'Invalid username or password')
         else:
             messages.error(request, 'Invalid username or password')
+    else:
+        form = AuthenticationForm()
     return render(request, 'registration/login.html', {'form': form})
+
+
+@login_required
+def home(request):
+    print('Home view accessed.')
+    return render(request, 'home.html')
+
 
 def redirect_to_login(request):
     if not request.user.is_authenticated:
